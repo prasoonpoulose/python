b504093f92114d30b8ba30815559b16b553f15ee
prasoonpoulose <prasoonpoulose@gmail.com>
2026-02-03 17:09:59 +0000

    updated the code

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 import os
 print ("Hello world")
-print(os.getenv("HOSTNAME"))
+print(os.getenv("HOSTNAME")) # env variable
 
-    
+if os.getenv("Hostname") == "DELL-LAPTOP":  # Conditional Statement
+   print("Its your Laptop")
+else:
+   print("Oh..Its not your laptop")
+
